d95e50540e6a49d3b05b9d38b8d3b188761500fb
Ed Summers <ehs@pobox.com>
2011-06-14 21:59:14 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// imports
+
+var sys = require('sys'),
+    redis = require('redis'),
+    io = require('socket.io'),
+    express = require('express'),
+    spawn = require('child_process').spawn;
+
+
+// set up the webapp
+
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'seachstream'
+  });
+});
+
+app.listen(3000);
+
+// io.socket configuration
+
+var socket = io.listen(app);
+socket.on('connect', function(client) {
+    console.log(client);
+});
+
+var wikipedia = redis.createClient();
+wikipedia.subscribe('wikipedia');
+wikipedia.on("message", function (channel, message) {
+    socket.broadcast(message);
+    console.log("channel: " + channel + " ; message: " + message);
+});
---
+++ updates.js
@@ -0,0 +1,36 @@
+var irc = require('irc-js'),
+    redis = require('redis');
+
+var client = new irc({
+    server: 'irc.wikimedia.org',
+    nick: 'wikistream',
+    log: true,
+    user: {
+        username: 'wikistream-bot',
+        realname: 'http://github.com/edsu/wikistream',
+    }
+})
+
+function parse_msg(s) {
+    m = /\x0314\[\[\x0307(.+?)\x0314\]\].+\x0302(http.+?)\x03.+\x0303(.+?)\x03.+\x0310(.+)\x03/.exec(s);
+    if (! m) { return null; } 
+    else {return {page: m[1], url: m[2], user: m[3], comment: m[4]}}
+}
+
+// publish wikipedia activity to redis
+
+db = redis.createClient();
+
+// connect to the database
+
+client.connect(function () {
+    client.join("#en.wikipedia");
+    client.on("privmsg", function (msg) {
+        m = parse_msg(msg.params[1]);
+        if (m) {
+            db.publish("wikipedia", JSON.stringify(m));
+            console.log(msg);
+            console.log(m);
+        }
+    });
+});
